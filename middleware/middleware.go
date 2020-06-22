1ea9a21b8c6e7f57d9ddb89079790dbcc7c728ca
Haris Chaniotakis <chanioxaris@gmail.com>
2020-06-23 00:33:22 +0300

    Introduce reCaptcha middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/chanioxaris/go-recaptcha"
+)
+
+// Middleware to handle Google reCaptcha verification.
+func Middleware(svc recaptcha.Service) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Get the reCaptcha token from default request body field 'g-recaptcha-response'.
+			captchaToken, err := svc.GetRequestToken(r)
+			if err != nil {
+				http.Error(w, "unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			// Verify the retrieved token.
+			if err := svc.Verify(captchaToken); err != nil {
+				http.Error(w, "unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
