1d189ea54b19c8cd433a35098c963285a690a5fc
Haris Chaniotakis <chanioxaris@gmail.com>
2020-09-28 21:04:19 +0300

    Refactor layout

--- middleware.go
+++ middleware.go
@@ -1,13 +1,11 @@
-package middleware
+package recaptcha
 
 import (
 	"net/http"
-
-	"github.com/chanioxaris/go-recaptcha/recaptcha"
 )
 
 // Middleware to handle Google reCaptcha verification.
-func Middleware(svc recaptcha.Service) func(http.Handler) http.Handler {
+func Middleware(svc Service) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// Get the reCaptcha token from default request body field 'g-recaptcha-response'.
